eb33d2bee4609306ddfad6b5d0de6ff08691c093
Lucas Belin <lucasbelinpro@gmail.com>
2022-11-16 19:23:36 +0100

    fix imports

--- backend/src/main/java/dev/lbelin/forumapi/repository/UserRepository.java
+++ backend/src/main/java/dev/lbelin/forumapi/repository/UserRepository.java
@@ -3,6 +3,8 @@ package dev.lbelin.forumapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import dev.lbelin.forumapi.model.User;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> { 
 
