be89541467a9fc5a41c435b5d7d40bfbd1efd69f
Lucas Belin <lucasbelinpro@gmail.com>
2022-11-18 13:20:14 +0100

    progress on register page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,21 +5,18 @@ function Login() {
     <div className="h-screen w-screen bg-zinc-800 flex justify-center items-center">
       <form
         action="submit"
-        className="bg-gradient-to-bl from-gray-900 to-black shadow-lg flex flex-col place-items-center h-fit pb-16 pt-8 px-8 rounded-lg min-w-[400px]"
+        className="form-gradient shadow-lg flex flex-col place-items-center h-fit pb-16 pt-8 px-8 rounded-lg min-w-[400px]"
       >
         <img src="/assets/logo.svg" alt="logo" className="w-[80px] h-[80px] mb-12" />
         <label htmlFor="username" className="place-self-start text-white">
           Username
         </label>
-        <input type="text" id="username" placeholder="Enter your username" className="login-input mb-5" />
+        <input type="text" id="username" placeholder="Enter your username" className="form-input mb-5" />
         <label htmlFor="password" className="place-self-start text-white">
           Password
         </label>
-        <input type="password" id="password" placeholder="Enter your password" className="login-input" />
-        <button
-          type="submit"
-          className="bg-zinc-800 text-white text-xl mt-8 px-8 py-2 rounded-lg mb-5 hover:bg-zinc-700 transition-all duration-200 ease-in-out"
-        >
+        <input type="password" id="password" placeholder="Enter your password" className="form-input" />
+        <button type="submit" className="form-submit">
           Login
         </button>
         <span className="text-white text-sm items-center flex flex-col">
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom"
+
 function Register() {
-  return <div>Register</div>
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+      <div className="form-gradient text-white min-w-[600px] flex rounded-lg">
+        <div className="px-32 flex flex-col items-center">
+          <img src="/assets/logo.svg" alt="logo" className="w-32 h-32" />
+          <p className="max-w-md text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quidem dolore ea beatae cumque cupiditate
+            nesciunt odit minus, soluta iste aperiam totam sed exercitationem magnam quia voluptas earum aut saepe!
+          </p>
+          <span className="text-white text-sm items-center flex flex-col">
+            Already have an account ? <br />
+            <Link to="/login" className="text-blue-600 underline hover:text-blue-500 transition-all ease-in-out">
+              Log in here
+            </Link>
+          </span>
+        </div>
+        <form action="submit">
+          <h1>Register</h1>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter your username" className="form-input" />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter your email" className="form-input" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" className="form-input" />
+          <label htmlFor="confirmPassword">Confirm password</label>
+          <input type="password" id="confirmPassword" placeholder="Confirm your password" className="form-input" />
+          <button type="submit" className="form-submit">
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
