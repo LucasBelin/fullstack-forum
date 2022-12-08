27d79a8da48cf083aecc0eadb8cbd8f48fb75f18
Lucas Belin <lucasbelinpro@gmail.com>
2022-12-08 14:05:30 +0100

    add twin macro

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
+import GlobalStyles from "./styles/GlobalStyles"
 
 axios.defaults.baseURL = "http://localhost:8080/api"
 axios.defaults.headers.post["Content-Type"] = "application/json"
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider authType={"cookie"} authName={"_auth"} cookieDomain={window.location.hostname} cookieSecure={false}>
       <BrowserRouter>
+        <GlobalStyles />
         <App />
       </BrowserRouter>
     </AuthProvider>
