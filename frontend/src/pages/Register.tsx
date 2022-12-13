558cfdded7affbfac61e525ac68086934e445937
Lucas Belin <lucasbelinpro@gmail.com>
2022-12-13 23:45:00 +0100

    redesign register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,10 @@
 import axios from "axios"
-import { FaForumbee } from "react-icons/fa"
+import React, { useState } from "react"
+import { AiOutlineMail, AiTwotoneEye, AiTwotoneEyeInvisible, AiTwotoneLock } from "react-icons/ai"
+import { FiAtSign } from "react-icons/fi"
 import { useMutation } from "react-query"
 import { Link, useNavigate } from "react-router-dom"
+import styled from "styled-components"
 import tw from "twin.macro"
 import { z } from "zod"
 
@@ -15,13 +18,13 @@ type RegistrationRequest = z.infer<typeof RegistrationRequest>
 const RegistrationResponse = z.object({
   username: z.string(),
   email: z.string().email(),
-  createdOn: z.string(),
-  updatedOn: z.string(),
+  createdOn: z.string().datetime(),
+  updatedOn: z.string().datetime(),
 })
 type RegistrationResponse = z.infer<typeof RegistrationResponse>
 
 const Container = tw.div`
-  h-screen w-screen bg-zinc-800 flex justify-center items-center
+  h-screen w-screen bg-slate-900 flex justify-center items-center
 `
 const RegisterForm = tw.form`
   bg-gradient-to-bl from-gray-900 to-black shadow-lg flex flex-col place-items-center h-fit pb-16 pt-8 px-8 rounded-lg min-w-[400px]
@@ -29,15 +32,18 @@ const RegisterForm = tw.form`
 const Label = tw.label`
   place-self-start text-white mt-5
 `
-const FormInput = tw.input`
-  text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400
-  text-white focus:ring-blue-500 focus:border-blue-500
+
+const Input = styled.input<{ isPassword?: boolean }>`
+  ${tw`rounded-none rounded-r-lg border block flex-1 min-w-0 w-full text-sm p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white`}
+  ${({ isPassword }) => isPassword && tw`rounded-none rounded-r-none`}
 `
 const SubmitButton = tw.button`
-  bg-zinc-800 text-white text-xl mt-8 px-8 py-2 rounded-lg mb-5 hover:bg-zinc-700 transition-all duration-200 ease-in-out
+  bg-slate-800 text-white text-xl mt-8 px-8 py-2 rounded-lg mb-5 hover:bg-slate-700 transition-all
+  duration-200 ease-in-out w-fit place-self-center
 `
 
 function Register() {
+  const [showPassword, setShowPassword] = useState(false)
   const navigate = useNavigate()
   const registerMutation = useMutation(
     ({ username, email, password }: RegistrationRequest) =>
@@ -49,29 +55,80 @@ function Register() {
     },
   )
 
+  const togglePasswordVisibility = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setShowPassword(prev => !prev)
+  }
+
   return (
     <Container>
-      <RegisterForm action="submit">
-        <div className="mb-7">
-          <FaForumbee size={80} color="white" />
-        </div>
+      <form
+        action="submit"
+        className="flex flex-col bg-gradient-to-bl from-gray-900 to-black border border-black shadow-lg py-8 px-16 rounded-md bg-opacity-5 h-[80%] max-w-xl"
+      >
+        <h1 className="text-white text-3xl text-center">Create your account</h1>
         <Label htmlFor="username" className="place-self-start text-white">
           Username
         </Label>
-        <FormInput type="text" id="username" placeholder="Enter your username" />
+        <div className="flex">
+          <span className="inline-flex items-center px-3 text-[20px] text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            <FiAtSign size={20} />
+          </span>
+          <Input type="text" id="username" placeholder="Jymmiah" />
+        </div>
+
         <Label htmlFor="email" className="place-self-start text-white">
           Email
         </Label>
-        <FormInput type="email" id="email" placeholder="Enter your email" />
+        <div className="flex">
+          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            <AiOutlineMail size={20} />
+          </span>
+          <input
+            type="text"
+            id="email"
+            className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="lorem@ipsum.com"
+          />
+        </div>
+
         <Label htmlFor="password" className="place-self-start text-white">
           Password
         </Label>
-        <FormInput type="password" id="password" placeholder="Enter your password" />
-        <Label htmlFor="confirmPassword" className="place-self-start text-white">
+        <div className="flex">
+          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            <AiTwotoneLock size={20} />
+          </span>
+          <Input isPassword type={showPassword ? "text" : "password"} id="password" placeholder="Your password" />
+          <button
+            onClick={togglePasswordVisibility}
+            className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+          >
+            {showPassword ? <AiTwotoneEye size={20} /> : <AiTwotoneEyeInvisible size={20} />}
+          </button>
+        </div>
+
+        <Label htmlFor="confirm-password" className="place-self-start text-white">
           Confirm password
         </Label>
-        <FormInput type="password" id="confirmPassword" placeholder="Confirm your password" />
-        <SubmitButton type="submit">Register</SubmitButton>
+        <div className="flex">
+          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            <AiTwotoneLock size={20} />
+          </span>
+          <Input
+            isPassword
+            type={showPassword ? "text" : "password"}
+            id="confirm-password"
+            placeholder="Just to make sure"
+          />
+          <button
+            onClick={togglePasswordVisibility}
+            className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+          >
+            {showPassword ? <AiTwotoneEye size={20} /> : <AiTwotoneEyeInvisible size={20} />}
+          </button>
+        </div>
+        <SubmitButton>Register</SubmitButton>
         <div className="px-32 flex flex-col items-center">
           <span className="text-white text-sm items-center flex flex-col">
             Already have an account ? <br />
@@ -80,7 +137,7 @@ function Register() {
             </Link>
           </span>
         </div>
-      </RegisterForm>
+      </form>
     </Container>
   )
 }
