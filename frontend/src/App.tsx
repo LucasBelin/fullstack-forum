775d300b952f62a6fa0b467e8ac41feefe42fdbe
Lucas Belin <lucasbelinpro@gmail.com>
2023-01-27 19:15:08 +0100

    add account registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { RequireAuth } from "react-auth-kit"
+import { Toaster } from "react-hot-toast"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
@@ -9,6 +10,7 @@ function App() {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster position="bottom-right" />
       <Routes>
         <Route
           path="/"
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,6 @@
-import axios from "axios"
-import React, { useState } from "react"
+import axios, { AxiosError } from "axios"
+import React, { useRef, useState } from "react"
+import { toast } from "react-hot-toast"
 import { AiOutlineMail, AiTwotoneEye, AiTwotoneEyeInvisible, AiTwotoneLock } from "react-icons/ai"
 import { FiAtSign } from "react-icons/fi"
 import { useMutation } from "react-query"
@@ -9,19 +10,11 @@ import tw from "twin.macro"
 import { z } from "zod"
 
 const RegistrationRequest = z.object({
-  username: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+  username: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z.string().optional(),
 })
 type RegistrationRequest = z.infer<typeof RegistrationRequest>
-
-const RegistrationResponse = z.object({
-  username: z.string(),
-  email: z.string().email(),
-  createdOn: z.string().datetime(),
-  updatedOn: z.string().datetime(),
-})
-type RegistrationResponse = z.infer<typeof RegistrationResponse>
 
 const Container = tw.div`
   h-screen w-screen bg-slate-900 flex justify-center items-center
@@ -42,22 +35,97 @@ const SubmitButton = tw.button`
   duration-200 ease-in-out w-fit place-self-center
 `
 
+const regexUsername = /^[a-zA-Z0-9_ -]{3,16}$/ // 3-16 characters, only letters, numbers, dashes, underscores, and spaces
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%&+=!])[A-Za-z\d@$!%*?&]{8,}$/ // 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
+
 function Register() {
   const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const [usernameError, setUsernameError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [passwordMismatchError, setPasswordMismatchError] = useState(false)
+
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
   const navigate = useNavigate()
   const registerMutation = useMutation(
     ({ username, email, password }: RegistrationRequest) =>
-      axios.post("/register", { username, email, password }).then(res => RegistrationResponse.parse(res.data)),
+      axios.post("/users/register", { username, email, password }),
     {
-      onSuccess: ({ username, email, createdOn, updatedOn }: RegistrationResponse) => {
+      onSuccess: () => {
         navigate("/login")
+      },
+      onError: err => {
+        handleError(err as AxiosError)
       },
     },
   )
 
+  const handleError = (err: AxiosError) => {
+    console.log("toast")
+    switch (err.response?.status) {
+      case 400:
+        toast.error("Make sure all fields are filled out correctly.")
+        break
+      case 409:
+        toast.error("That username or email is already in use.")
+        break
+      default:
+        toast.error("Something went wrong. Please try again later.")
+        break
+    }
+  }
+
   const togglePasswordVisibility = (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault()
     setShowPassword(prev => !prev)
+  }
+
+  const toggleConfirmPasswordVisibility = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setShowConfirmPassword(prev => !prev)
+  }
+
+  const validateUsername = (e: React.FormEvent<HTMLInputElement>) => {
+    const username = e.currentTarget.value
+    if (regexUsername.test(username)) {
+      setUsernameError(false)
+      return
+    }
+    setUsernameError(true)
+  }
+
+  const validateEmail = (e: React.FormEvent<HTMLInputElement>) => {
+    const email = e.currentTarget.value
+    if (regexEmail.test(email)) {
+      setEmailError(false)
+      return
+    }
+    setEmailError(true)
+  }
+
+  const validatePassword = (e: React.FormEvent<HTMLInputElement>) => {
+    const password = e.currentTarget.value
+    if (regexPassword.test(password)) {
+      setPasswordError(false)
+      return
+    }
+    setPasswordError(true)
+  }
+
+  const validateConfirmPassword = (e: React.FormEvent<HTMLInputElement>) => {
+    const password = passwordRef.current?.value
+    const confirmPassword = e.currentTarget.value
+    if (password === confirmPassword) {
+      setPasswordMismatchError(false)
+      return
+    }
+    setPasswordMismatchError(true)
   }
 
   return (
@@ -67,42 +135,65 @@ function Register() {
         <Label htmlFor="username" className="place-self-start text-white">
           Username
         </Label>
-        <div className="flex">
-          <span className="inline-flex items-center px-3 text-[20px] text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-            <FiAtSign size={20} />
-          </span>
-          <Input type="text" id="username" placeholder="Username" />
+        <div>
+          <div className="flex">
+            <span className="inline-flex items-center px-3 text-[20px] text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+              <FiAtSign size={20} />
+            </span>
+            <Input
+              type="text"
+              id="username"
+              placeholder="Username"
+              onBlur={e => validateUsername(e)}
+              ref={usernameRef}
+            />
+          </div>
+          {usernameError ? (
+            <span className="text-red-600 text-sm ml-2">The username doesn't meet the requirements.</span>
+          ) : null}
         </div>
 
         <Label htmlFor="email" className="place-self-start text-white">
           Email
         </Label>
-        <div className="flex">
-          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-            <AiOutlineMail size={20} />
-          </span>
-          <input
-            type="text"
-            id="email"
-            className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="john-doe@gmail.com"
-          />
+        <div>
+          <div className="flex">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+              <AiOutlineMail size={20} />
+            </span>
+            <Input type="text" id="email" placeholder="john-doe@gmail.com" onBlur={validateEmail} ref={emailRef} />
+          </div>
+          {emailError ? (
+            <span className="text-red-600 text-sm ml-2">The email doesn't meet the requirements.</span>
+          ) : null}
         </div>
 
         <Label htmlFor="password" className="place-self-start text-white">
           Password
         </Label>
-        <div className="flex">
-          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-            <AiTwotoneLock size={20} />
-          </span>
-          <Input isPassword type={showPassword ? "text" : "password"} id="password" placeholder="Your password" />
-          <button
-            onClick={togglePasswordVisibility}
-            className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
-          >
-            {showPassword ? <AiTwotoneEye size={20} /> : <AiTwotoneEyeInvisible size={20} />}
-          </button>
+        <div>
+          <div className="flex">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+              <AiTwotoneLock size={20} />
+            </span>
+            <Input
+              isPassword
+              ref={passwordRef}
+              type={showPassword ? "text" : "password"}
+              id="password"
+              placeholder="Your password"
+              onBlur={validatePassword}
+            />
+            <button
+              onClick={togglePasswordVisibility}
+              className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+            >
+              {showPassword ? <AiTwotoneEye size={20} /> : <AiTwotoneEyeInvisible size={20} />}
+            </button>
+          </div>
+          {passwordError ? (
+            <span className="text-red-600 text-sm ml-2">The password doesn't meet the requirements.</span>
+          ) : null}
         </div>
 
         <Label htmlFor="confirm-password" className="place-self-start text-white">
@@ -114,18 +205,31 @@ function Register() {
           </span>
           <Input
             isPassword
-            type={showPassword ? "text" : "password"}
+            type={showConfirmPassword ? "text" : "password"}
             id="confirm-password"
             placeholder="Confirm password"
+            onBlur={validateConfirmPassword}
           />
           <button
-            onClick={togglePasswordVisibility}
+            onClick={toggleConfirmPasswordVisibility}
             className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
           >
-            {showPassword ? <AiTwotoneEye size={20} /> : <AiTwotoneEyeInvisible size={20} />}
+            {showConfirmPassword ? <AiTwotoneEye size={20} /> : <AiTwotoneEyeInvisible size={20} />}
           </button>
         </div>
-        <SubmitButton>Register</SubmitButton>
+        {passwordMismatchError ? <span className="text-red-600 text-sm ml-2">The passwords don't match.</span> : null}
+        <SubmitButton
+          onClick={e => {
+            e.preventDefault()
+            registerMutation.mutate({
+              username: usernameRef.current?.value,
+              email: emailRef.current?.value,
+              password: passwordRef.current?.value,
+            })
+          }}
+        >
+          Register
+        </SubmitButton>
         <div className="px-32 flex flex-col items-center">
           <span className="text-white text-sm items-center flex flex-col">
             Already have an account ? <br />
